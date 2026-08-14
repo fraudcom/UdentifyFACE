75bbabf1dd9dd30625193bcb56ddcefed19fd907
SercanCobanoglu <131464765+SercanCobanoglu@users.noreply.github.com>
2026-08-14 11:53:06 +0300

    Update Package.swift
    Package.swift has been updated

--- Package.swift
+++ Package.swift
@@ -12,14 +12,14 @@ let package = Package(
              targets: ["UdentifyFACE"]),  // Point directly to the binary target
      ],
      dependencies: [
-        .package(url: "https://github.com/fraudcom/UdentifyCommons.git", exact: "26.1.3"),
+        .package(url: "https://github.com/fraudcom/UdentifyCommons.git", exact: "26.3.0814"),
          .package(url: "https://github.com/airbnb/lottie-spm.git", exact: "4.5.0"),
      ],
      targets: [
          .binaryTarget(
              name: "UdentifyFACE",
-             url: "https://api.github.com/repos/fraudcom/mobile/releases/assets/397880893.zip",
-             checksum: "2783e02aea8badb079348140898e3cc01141c138008929cd03766db96875d898"
+             url: "https://api.github.com/repos/fraudcom/mobile/releases/assets/514147436.zip",
+             checksum: "2b9782a22f38171672a9b58b066d568baab5d329e54df6fb9e19a81029dced5d"
          )
      ]
  )
